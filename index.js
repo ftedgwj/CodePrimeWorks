c410de61cf423f71f68a8d327badc230f060e247
ftedgwj <bsjcigvmartin77@hotmail.com>
2024-05-11 07:57:56 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-function reorderList(head) {
-  if (!head || !head.next) return;
-  let slow = head;
-  let fast = head;
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
+function isMatch(s, p) {
+  const dp = Array.from(Array(s.length + 1), () =>
+    Array(p.length + 1).fill(false),
+  );
+  dp[0][0] = true;
+  for (let i = 1; i <= p.length; i++) {
+    if (p[i - 1] === "*") {
+      dp[0][i] = dp[0][i - 2];
+    }
   }
-  let prev = null;
-  let curr = slow.next;
-  slow.next = null;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 1; j <= p.length; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === "*") {
+        dp[i][j] = dp[i][j - 2];
+        if (p[j - 2] === "." || s[i - 1] === p[j - 2]) {
+          dp[i][j] = dp[i][j] || dp[i - 1][j];
+        }
+      }
+    }
   }
-  let first = head;
-  let second = prev;
-  while (second) {
-    const nextFirst = first.next;
-    const nextSecond = second.next;
-    first.next = second;
-    second.next = nextFirst;
-    first = nextFirst;
-    second = nextSecond;
-  }
+  return dp[s.length][p.length];
 }
