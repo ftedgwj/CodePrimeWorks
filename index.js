c964898992f366a55f7e622fc7696abbf272521c
ftedgwj <bsjcigvmartin77@hotmail.com>
2024-05-14 23:26:51 +0700

    start project

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-function isMatch(s, p) {
-  const dp = Array.from(Array(s.length + 1), () =>
-    Array(p.length + 1).fill(false),
-  );
-  dp[0][0] = true;
-  for (let i = 1; i <= p.length; i++) {
-    if (p[i - 1] === "*") {
-      dp[0][i] = dp[0][i - 2];
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack(0, [], 0);
+  return result;
+  function backtrack(start, current, sum) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target || start === candidates.length) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      backtrack(i, current, sum + candidates[i]);
+      current.pop();
     }
   }
-  for (let i = 1; i <= s.length; i++) {
-    for (let j = 1; j <= p.length; j++) {
-      if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
-        dp[i][j] = dp[i - 1][j - 1];
-      } else if (p[j - 1] === "*") {
-        dp[i][j] = dp[i][j - 2];
-        if (p[j - 2] === "." || s[i - 1] === p[j - 2]) {
-          dp[i][j] = dp[i][j] || dp[i - 1][j];
-        }
-      }
-    }
-  }
-  return dp[s.length][p.length];
 }
